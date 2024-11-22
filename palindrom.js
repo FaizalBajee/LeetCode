0dc807b6495256be3e56fee2302e7b5e4639a7ae
faizalj <faizalbajee00@gmail.com>
2024-11-22 11:48:38 +0530

    palindrom modified

--- palindrom.js
+++ palindrom.js
@@ -1,9 +1,24 @@
-var isPalindrome = function(x) {
-    let rev=x.toString().split('').reverse().join('');
-    console.log(rev)
-    if(x == rev){
-        return true;
+// var isPalindrome = function(x) {
+//     let rev=x.toString().split('').reverse().join('');
+//     console.log(rev)
+//     if(x == rev){
+//         return true;
+//     }
+//     return false
+// };
+function isPalindrome(num) {
+    let x = num.toString()
+    low = 0;
+    high = x.length - 1;
+    for (let i = 0; i < x.length / 2; i++) {
+        if (x[low] !== x[high]) {
+            return false;
+        }
+        low++;
+        high--;
     }
-    return false
-};
-isPalindrome(121)
+    return true;
+}
+// isPalindrome(12231)
+console.log(isPalindrome(4334))
+//first want to check failure cases 

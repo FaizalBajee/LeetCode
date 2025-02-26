a7bad218690cd6da0d5203641654bf403c1d160c
faizalj <faizalbajee00@gmail.com>
2025-02-26 23:48:58 +0530

    reduce folder added

--- Examples/reduce/reduce.js
+++ Examples/reduce/reduce.js
@@ -2,7 +2,7 @@ let arr = ['apple', 'banana', 'grape', 'grape', 'grape', 'banana', 'apple', 'app
 
 const ans = arr.reduce((acc, words) => {
     acc[words] = (acc[words] || 0) + 1;
-    return acc
+    return acc;
 }, {})
 
 console.log(ans)

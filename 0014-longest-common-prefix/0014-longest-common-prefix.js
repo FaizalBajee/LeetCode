ae01061c6a06dd2d9b69cfe2da1e2b0805e98615
Faizal Bajee <161807564+FaizalBajee@users.noreply.github.com>
2025-11-07 03:35:09 +0530

    Clean up commented code in longestCommonPrefix.js
    Removed commented-out code for clarity.

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -16,3 +16,32 @@ var longestCommonPrefix = function (strs) {
     return prefix
 
 };
+
+
+// var longestCommonPrefix = function (strs) {
+//     let prefix = ""
+
+//     for (let i = 0; i < strs.length; i++) {
+//         let split = strs[i + 1]
+
+//         for (let j = i + 1; j < split.length; j++) {
+//             let split1 = strs[i]
+//             let split2 = strs[j]
+//             //  console.log(split2)
+//             if (strs.length == 1 && split[i]) {
+//                 prefix += split[i]
+//             }
+//             if (split1.length == 0) return prefix
+//             if (split1[i] !== split2[i]) return prefix
+
+//             if (split1[i] === split2[i] && split1[0] === split2[0] && !prefix.includes(split1[i])) {
+//                 prefix += split1[i]
+//             }
+
+//         }
+
+//     }
+//     console.log(prefix)
+//     return prefix
+
+// };

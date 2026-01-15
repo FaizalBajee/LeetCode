f36edb22c0ba1e23781f47d3c7129e89a529d2e0
Faizal Bajee <161807564+FaizalBajee@users.noreply.github.com>
2026-01-15 20:22:30 +0530

    Time: 0 ms (100%), Space: 53.8 MB (71.72%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -5,12 +5,14 @@
  */
 var removeElement = function (nums, val) {
 
+    let k = 0;
+
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == val) {
-            nums.splice(i, 1);
-            i--;
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
         }
     }
-    console.log(nums);
 
-}
+    return k;
+};

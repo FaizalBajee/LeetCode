370bc8ff46a8cbf508861203f33893011cb6896e
faizalbajee <faizalbajee00@gmail.com>
2026-01-20 12:27:02 +0530

    move concept fils to engineering playbook repo

--- Examples/recursive.js
+++
@@ -1,6 +0,0 @@
-function recursive(num) {
-    if (num === 1) return 1
-    return num + recursive(num - 1);
-}
-console.log(recursive(3))
-
--- Examples/reduce/reduce.js
+++
@@ -1,8 +0,0 @@
-let arr = ['apple', 'banana', 'grape', 'grape', 'grape', 'banana', 'apple', 'apple']
-
-const ans = arr.reduce((acc, words) => {
-    acc[words] = (acc[words] || 0) + 1;
-    return acc;
-}, {})
-
-console.log(ans)
